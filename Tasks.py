2fe8a2106b27d7236d845746b458bd198939572c
donia haj nayef <doniahajnayef@gmail.com>
2022-08-01 19:36:26 +0200

    tasks file

---
+++ Tasks.py
@@ -0,0 +1,59 @@
+#1
+s = true
+
+#2
+y = 5
+
+#3
+z = 3.3
+
+#4
+S = "donia"
+
+#5 
+float(y)
+
+#6
+yes, we can convert the string to int when it is just number,
+but when it is name we cann't convert.
+
+
+#7
+l =[1,2,3,4,5,]
+
+#8
+
+t =(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)
+
+
+#9
+
+l=(1,2,3,4,5)
+
+#10
+
+d ={'donia':10, 'kawthar':8 , 'mawia':5}
+
+#11
+yes, we can but we don't have to use it.
+
+
+#12
+
+
+#13
+'''
+low level:
+The language of the computer
+It consists of 1 , 0
+It is easy to be Executed
+It is hard to program
+
+high level
+Easy to program
+Takes less time to be coded
+Easy to be corrected
+
+'''
+
+
